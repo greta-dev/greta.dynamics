497ec53073f9b7af56d7d87b962055c93e8b56c4
jdyen <jdl.yen@gmail.com>
2017-08-24 08:00:38 +1000

    update namespace

--- R/dynamics.R
+++ R/dynamics.R
@@ -29,28 +29,11 @@ tf_iterate_state <- function (mat, state, niter) {
   states <- list(state)
 
   # iterate the matrix
-  for (i in seq_len(niter))
+  for (i in seq_len(max(niter)))
     states[[i + 1]] <-  tf$matmul(mat, states[[i]], transpose_a = TRUE)
 
   # return the final growth rate (should be same for all states at convergence)
-  states[[niter + 1]]
-
-}
-
-# iterate matrix tensor `mat` `niter` times, each time using and updating vector
-# tensor `state`, and return the final state
-tf_iterate_state_seq <- function (mat, state, niter) {
-
-  # store states (can't overwrite since we need to maintain the chain of nodes)
-  states <- list(state)
-
-  # iterate the matrix
-  iter_tmp <- ifelse(length(niter) > 1, niter, seq_len(niter))
-  for (i in iter_tmp)
-    states[[i + 1]] <-  tf$matmul(mat, states[[i]], transpose_a = TRUE)
-
-  # return the final growth rate (should be same for all states at convergence)
-  states
+  do.call(greta::.internals$tensors$tf_cbind, states[niter + 1])
 
 }
 
@@ -160,7 +143,7 @@ iterate_state <- function(matrix, state, niter) {
             call. = FALSE)
 
     # output dimensions
-    state_dim
+    c(state_dim[1], length(niter))
   }
 
   op('iterate_state',
@@ -168,52 +151,6 @@ iterate_state <- function(matrix, state, niter) {
      state,
      operation_args = list(niter = niter),
      tf_operation = tf_iterate_state,
-     dimfun = dimfun)
-
-}
-
-#' @name iterate_state_seq
-#' @rdname gretaDynamics
-#'
-#' @param matrix a square, two-dimensional (i.e. matrix-like) greta array
-#'   representing transition probabilities between states
-#' @param state a column vector greta array representing the initial state from
-#'   which to iterate the matrix
-#' @param niter a vector of positive integers giving the time steps at which to
-#'   calculate the iterated state
-#'
-#' @export
-iterate_state_seq <- function(matrix, state, niter) {
-
-  niter <- as.integer(niter)
-
-  dimfun <- function(elem_list) {
-
-    # input dimensions
-    matrix_dim <- dim(elem_list[[1]])
-    state_dim <- dim(elem_list[[2]])
-
-    if (length(state_dim) != 2 | state_dim[2] != 1)
-      stop ('state must be a column vector greta array',
-            call. = FALSE)
-
-    if (length(matrix_dim) != 2 | matrix_dim[1] != matrix_dim[2])
-      stop ('matrix must be a two-dimensional square greta array',
-            call. = FALSE)
-
-    if (matrix_dim[2] != state_dim[1])
-      stop ('number of elements in state must match the dimension of matrix',
-            call. = FALSE)
-
-    # output dimensions
-    c(state_dim[1], length(niter))
-  }
-
-  op('iterate_state_seq',
-     matrix,
-     state,
-     operation_args = list(niter = niter),
-     tf_operation = tf_iterate_state_seq,
      dimfun = dimfun)
 
 }
