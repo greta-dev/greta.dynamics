8ad4e2eb68a5a51bad8bf733f643e9644147ab30
Nick Golding <goldingn35@gmail.com>
2018-10-02 16:16:30 +1000

    redesign iterate_matrix

--- R/iterate.R
+++ R/iterate.R
@@ -1,245 +1,199 @@
-# methods for using stage-structured (matrix) population models using greta
-# dynamics module
+#' @name iterate_matrix
+#'
+#' @title iterate transition matrices
+#'
+#' @description calculate the intrinsic growth rate(s) and stable stage
+#'   distribution(s) for a stage-structured dynamical system, encoded as a
+#'   transition matrix
+#'
+#' @details \code{iterate_matrix} can either act on single transition matrix and
+#'   initial state (if \code{matrix} is 2D and \code{initial_state} is a column
+#'   vector), or it can simultaneously act on \emph{n} different matrices and/or
+#'   \emph{n} different initial states (if \code{matrix} and
+#'   \code{initial_state} are 3D arrays). In the latter case, the first
+#'   dimension of both objects should be the batch dimension \emph{n}.
+#'
+#' @import greta
+#' @importFrom tensorflow tf shape
+#'
+#' @param matrix either a square 2D transition matrix (with dimensions m x m),
+#'   or a 3D array (with dimensions n x m x m), giving one or more transition
+#'   matrices to iterate
+#' @param initial_state either a column vector (with m elements) or a 3D array
+#'   (with dimensions n x m x 1) giving one or more initial states from which to
+#'   iterate the matrix
+#' @param niter a positive integer giving the number of times to iterate the
+#'   matrix
+#'
+#' @return a named list with two greta arrays: \code{lambda} a scalar or vector
+#'   giving the ratio of the first stage values between the final two
+#'   iterations, and \code{final_state} a vector or matrix (with the same
+#'   dimensions as \code{initial_state}) giving the state after the final
+#'   iteration. If the system has convreged in \code{niter} iterations, these
+#'   correspond to the asymptotic growth rate and stable stage distribution
+#'   respectively.
+#'
+#' @export
+#'
+#' @examples
+#'
+#'
+iterate_matrix <- function(matrix,
+                           initial_state = rep(1, ncol(matrix)),
+                           niter = 100) {
+
+  niter <- as.integer(niter)
+  matrix <- as.greta_array(matrix)
+  state <- as.greta_array(initial_state)
+
+  # check input dimensions
+  matrix_dim <- dim(matrix)
+  state_dim <- dim(state)
+  matrix_n_dim <- length(matrix_dim)
+  state_n_dim <- length(state_dim)
+
+  if (!matrix_n_dim %in% 2:3 | !state_n_dim %in% 2:3) {
+    stop ("matrix and state must be either two- or three-dimensional",
+          call. = FALSE)
+  }
+
+  # ensure the last dim of state is 1
+  if (state_dim[state_n_dim] != 1) {
+    stop ("initial_state must be either a column vector, ",
+          "or a 3D array with final dimension 1",
+          call. = FALSE)
+  }
+
+  # if this is multisite
+  matrix_multisite <- matrix_n_dim == 3
+  state_multisite <- state_n_dim == 3
+  multisite <- matrix_multisite | state_multisite
+
+  # ensure the site dimension matches
+  if (multisite) {
+
+    if (!state_multisite) {
+
+      n <- matrix_dim[1]
+
+      # expand state
+      state_list <- replicate(n, state, simplify = FALSE)
+      state <- t(do.call(cbind, state_list))
+
+      # add trailing dimension back on
+      dim(state) <- c(dim(state), 1)
+
+      state_dim <- dim(state)
+      state_n_dim <- 3
+
+    }
+
+    if (!matrix_multisite) {
+
+      n <- state_dim[1]
+      matrix2 <- matrix
+
+      # expand matrix
+      dim(matrix2) <- c(1, dim(matrix2))
+      matrix_list <- replicate(n, matrix2, simplify = FALSE)
+      matrix <- do.call(abind, c(matrix_list, list(along = 1)))
+
+      matrix_dim <- dim(matrix)
+      matrix_n_dim <- 3
+
+    }
+
+    if (matrix_multisite & state_multisite) {
+      n <- matrix_dim[1]
+      if (state_dim[1] != n) {
+        stop ("if matrix is 3D and initial_state is a matrix",
+              "the batch dimension (n) must match",
+              call. = FALSE)
+      }
+    }
+
+  } else {
+    n <- 1
+  }
+
+  # check the number of stages matches
+  m <- matrix_dim[2]
+  if (multisite) {
+    matrix_raw_dim <- matrix_dim[2:3]
+    state_raw_dim <- state_dim[2]
+  } else {
+    matrix_raw_dim <- matrix_dim
+    state_raw_dim <- state_dim[1]
+  }
+
+  if (!all(matrix_raw_dim == m)) {
+    stop ("each matrix must be a two-dimensional square greta array ",
+          call. = FALSE)
+  }
+
+  if (state_raw_dim != m) {
+    stop ("length of each initial_state must match the dimension of matrix",
+          call. = FALSE)
+  }
+
+  # op returning a fake greta array which is actually a list containing both
+  # values and states
+  states <- op('iterate_matrix',
+               matrix,
+               state,
+               operation_args = list(niter = niter),
+               tf_operation = "tf_iterate_matrix",
+               dim = c(1, 1))
+
+  # ops to extract the two components
+  lambda <- op('lambda',
+               states,
+               tf_operation = "tf_extract_lambda",
+               dim = c(n, 1))
+
+  final_state <- op('final_state',
+                     states,
+               tf_operation = "tf_extract_final_state",
+               dim = state_dim)
+
+  list(lambda = lambda,
+       final_state = final_state)
+
+}
 
 op <- greta::.internals$nodes$constructors$op
+as.greta_array <- greta::.internals$greta_arrays$as.greta_array
 
 # tensorflow code
 # iterate matrix tensor `mat` `niter` times, each time using and updating vector
 # tensor `state`, and return lambda for the final iteration
-tf_iterate_lambda <- function (mat, state, niter) {
+tf_iterate_matrix <- function (mat, state, niter) {
 
-  # store states (can't overwrite since we need to maintain the chain of nodes)
   states <- list(state)
 
-  # iterate the matrix
+  # iterate the matrix, storing states in a list
   for (i in seq_len(niter))
     states[[i + 1]] <-  tf$matmul(mat, states[[i]], transpose_a = TRUE)
 
-  # return the final growth rate (should be same for all states at convergence)
-  lambda <- states[[niter + 1]][1] / states[[niter]][1]
-  tf$reshape(lambda, shape = c(1L, 1L))
+  # return the last two states
+  states[niter + 0:1]
 
 }
 
-# iterate matrix tensor `mat` `niter` times, each time using and updating vector
-# tensor `state`, and return the final state
-tf_iterate_state <- function (mat, state, niter) {
+# return the ratio of the first stage values for the last two states, which
+# should be the intrinsic growth rate if the iteration has converged
+tf_extract_lambda <- function (states) {
 
-  # store states (can't overwrite since we need to maintain the chain of nodes)
-  states <- list(state)
-
-  # iterate the matrix
-  for (i in seq_len(niter))
-    states[[i + 1]] <-  tf$matmul(mat, states[[i]], transpose_a = TRUE)
-
-  # return the final growth rate (should be same for all states at convergence)
-  states[[niter + 1]]
-
-}
-
-# apply iterate_lambda to a series of n matrices of dimension m, stored as an n
-# x m^2 matrix, each row being unpacked *rowwise*
-tf_iterate_lambda_vectorised <- function (mat, state, n, m, niter) {
-
-  # create indices for a block-diagonal sparse matrix
-  # column wise offset to move the matrices along one horizontally
-  offset <- rep(seq_len(n) - 1, each = m ^ 2) * m
-  idx_cols <- rep(seq_len(m), m * n) + offset
-  idx_rows <- rep(rep(seq_len(m), each = m), n) + offset
-
-  # set up sparse tensor
-  indices <- tf$constant(cbind(idx_rows, idx_cols) - 1, dtype = tf$int64)
-  values <- tf$reshape(mat, shape = shape(n * m ^ 2))
-  shape <- tf$constant(as.integer(rep(n * m, 2)), dtype = tf$int64)
-  full_mat <- tf$SparseTensor(indices = indices,
-                              values = values,
-                              dense_shape = shape)
-
-  # replicate state for all patches
-  state <- tf$tile(state,
-                   tf$constant(c(n, 1L),
-                               dtype = tf$int32,
-                               shape = shape(2)))
-
-  # store states (can't overwrite since we need to maintain the chain of nodes)
-  states <- list(state)
-
-  # iterate the matrix
-  for (i in seq_len(niter))
-    states[[i + 1]] <-  tf$sparse_tensor_dense_matmul(full_mat, states[[i]])
-
-  # indices to subset the first state for each patch
-  idx_begin <- tf$constant(c(0L, 0L), dtype = tf$int32)
-  idx_end <- tf$constant(c(m * n, 1L), dtype = tf$int32)
-  idx_strides <- tf$constant(c(m, 1L), dtype = tf$int32)
-
-  # subset these iterated states to get the first state per sub-matrix
-  before <- tf$strided_slice(states[[niter]],
-                             begin = idx_begin,
-                             end = idx_end,
-                             strides = idx_strides)
-
-  after <- tf$strided_slice(states[[niter + 1]],
-                            begin = idx_begin,
-                            end = idx_end,
-                            strides = idx_strides)
-
-  # get growth rate
-  lambdas <- after / before
-  lambdas
-
-}
-
-#' @name greta.dynamics
-#' @title iterate transition matrices
-#'
-#' @description greta functions to calculate the intrinsic growth rate or stable
-#'   stage distribution for stage-structured dynamical systems, encoded as greta
-#'   arrays representing transition matrices
-#'
-#' @details \code{iterate_lambda} iterates a matrix a certain number of
-#'   times and returns, as a scalar greta array, the terminal growth rate for
-#'   the first element of the state vector. \code{iterate_state} carries out the
-#'   same procedure, but returns the final state vector.
-#'   \code{iterate_lambda_vectorised} is a vectorised version of
-#'   \code{iterate_lambda} for iterating over multiple matrices, returning a
-#'   vector of growth rates.
-#'
-#' @import greta
-#' @importFrom tensorflow tf shape
-NULL
-
-#' @name iterate_state
-#' @rdname greta.dynamics
-#'
-#' @param matrix a square, two-dimensional (i.e. matrix-like) greta array
-#'   representing transition probabilities between states
-#' @param state a column vector greta array representing the initial state from
-#'   which to iterate the matrix
-#' @param niter a positive integer giving the number of times to iterate the
-#'   matrix
-#'
-#' @export
-iterate_state <- function(matrix, state, niter) {
-
-  niter <- as.integer(niter)
-
-  dimfun <- function(elem_list) {
-
-    # input dimensions
-    matrix_dim <- dim(elem_list[[1]])
-    state_dim <- dim(elem_list[[2]])
-
-    if (length(state_dim) != 2 | state_dim[2] != 1)
-      stop ('state must be a column vector greta array',
-            call. = FALSE)
-
-    if (length(matrix_dim) != 2 | matrix_dim[1] != matrix_dim[2])
-      stop ('matrix must be a two-dimensional square greta array',
-            call. = FALSE)
-
-    if (matrix_dim[2] != state_dim[1])
-      stop ('number of elements in state must match the dimension of matrix',
-            call. = FALSE)
-
-    # output dimensions
-    state_dim
+  # handle possible site dimension
+  if (length(dim(states[[1]])) == 4) {
+    lambda <- states[[2]][, , 0, 0] / states[[1]][, , 0, 0]
+  } else {
+    lambda <- states[[2]][, 0, 0] / states[[1]][, 0, 0]
   }
-
-  op('iterate_state',
-     matrix,
-     state,
-     operation_args = list(niter = niter),
-     tf_operation = tf_iterate_state,
-     dimfun = dimfun)
-
+  lambda
 }
 
-#' @name iterate_lambda
-#' @rdname greta.dynamics
-#' @export
-iterate_lambda <- function(matrix, state, niter) {
-
-  niter <- as.integer(niter)
-
-  dimfun <- function(elem_list) {
-
-    # input dimensions
-    matrix_dim <- dim(elem_list[[1]])
-    state_dim <- dim(elem_list[[2]])
-
-    if (length(state_dim) != 2 | state_dim[2] != 1)
-      stop ('state must be a column vector greta array',
-            call. = FALSE)
-
-    if (length(matrix_dim) != 2 | matrix_dim[1] != matrix_dim[2])
-      stop ('matrix must be a two-dimensional square greta array',
-            call. = FALSE)
-
-    if (matrix_dim[2] != state_dim[1])
-      stop ('number of elements in state must match the dimension of matrix',
-            call. = FALSE)
-
-    # output dimensions
-    c(1, 1)
-  }
-
-  op('iterate_lambda',
-     matrix,
-     state,
-     operation_args = list(niter = niter),
-     tf_operation = tf_iterate_lambda,
-     dimfun = dimfun)
-
-}
-
-#' @name iterate_lambda_vectorised
-#' @rdname greta.dynamics
-#'
-#' @param matrices a rectangular two-dimensional greta array of dimension n x
-#'   m^2, each row of which gives the rowwise elements of a different m x m
-#'   matrix to iterate
-#' @param n the number of m x m matrices to be iterated (first dimensions of
-#'   \code{matrices})
-#' @param m the dimension of each matrix to be iterated
-#'
-#' @export
-iterate_lambda_vectorised <- function(matrices, state, n, m, niter) {
-
-  n <- as.integer(n)
-  m <- as.integer(m)
-  niter <- as.integer(niter)
-
-  dimfun <- function(elem_list) {
-
-    # input dimensions
-    matrices_dim <- dim(elem_list[[1]])
-    state_dim <- dim(elem_list[[2]])
-
-    if (length(state_dim) != 2 | state_dim[2] != 1)
-      stop ('state must be a column vector greta array',
-            call. = FALSE)
-
-    if (m != state_dim[1])
-      stop ('number of elements in state must match the dimension of matrix',
-            call. = FALSE)
-
-    if (length(matrices_dim) != 2 | matrices_dim[2] != (m ^ 2) | matrices_dim[1] != n)
-      stop ('matrix must be a rectangular greta array with dimensions n x m^2',
-            call. = FALSE)
-
-    # output dimensions
-    c(n, 1)
-
-  }
-
-  op('iterate_lambda_vectorised',
-     matrices,
-     state,
-     operation_args = list(n = n,
-                           m = m,
-                           niter = niter),
-     tf_operation = tf_iterate_lambda_vectorised,
-     dimfun = dimfun)
-
+# return the final state from matrix iteration (should have stabilised)
+tf_extract_final_state <- function (states) {
+  states[[2]]
 }
--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -7,38 +7,21 @@ set.seed(2017-05-01)
 
 dag_class <- greta::.internals$inference$dag_class
 
-# evaluate a greta_array, node, or tensor
-grab <- function (x) {
-
-  if (inherits(x, "node"))
-    x <- as.greta_array(x)
-
-  if (inherits(x, "greta_array")) {
-    dag <- dag_class$new(list(x))
-    x$node$define_tf(dag)
-    x <- get(dag$tf_name(x$node),
-             envir = dag$tf_environment)
-  }
-
-  tf$Session()$run(x)
-
-}
-
 randu <- function (...) {
   dim <- c(...)
   array(runif(prod(dim)), dim = dim)
 }
 
 # R versions of dynamics module methods
-it_lambda <- function (matrix, state, niter) {
+r_iterate_matrix <- function (matrix, state, niter) {
   states <- list(state)
   for (i in seq_len(niter))
     states[[i + 1]] <- states[[i]] %*% matrix
-  states[[niter + 1]][1] / states[[niter]][1]
+
+  lambda <- states[[niter + 1]][1] / states[[niter]][1]
+  final_state <- t(states[[niter + 1]])
+
+  list(lambda = lambda,
+       final_state = final_state)
 }
 
-it_state <- function (matrix, state, niter) {
-  for (i in seq_len(niter))
-    state <- state %*% matrix
-  state[1, ]
-}
